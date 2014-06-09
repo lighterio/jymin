0ff588fbdc0bf90f396cdcea3219b1915e4d8b88
Sam Eubank <sameubank+zerious@gmail.com>
2014-06-09 13:44:42 -0700

    Adding to the request object in getResponse

--- jymin.js
+++ jymin.js
@@ -1,9 +1,9 @@
 /**
- *      _                 _                ___   ____    ___
- *     | |_   _ _ __ ___ (_)_ __   __   __/ _ \ |___ \  / _ \
- *  _  | | | | | '_ ` _ \| | '_ \  \ \ / / | | |  __) || | | |
- * | |_| | |_| | | | | | | | | | |  \ V /| |_| | / __/ | |_| |
- *  \___/ \__, |_| |_| |_|_|_| |_|   \_/  \___(_)_____(_)___/
+ *      _                 _                ___   ____    ____
+ *     | |_   _ _ __ ___ (_)_ __   __   __/ _ \ |___ \  |___ \
+ *  _  | | | | | '_ ` _ \| | '_ \  \ \ / / | | |  __) |   __) |
+ * | |_| | |_| | | | | | | | | | |  \ V /| |_| | / __/ _ / __/
+ *  \___/ \__, |_| |_| |_|_|_| |_|   \_/  \___(_)_____(_)_____|
  *        |___/
  *
  * http://lighter.io/jymin
@@ -30,7 +30,7 @@
  */
 
 
-this.jymin = {version: '0.2.0'};
+this.jymin = {version: '0.2.2'};
 
 /**
  * Empty handler.
@@ -44,18 +44,18 @@ var globalResponseFailureHandler = doNothing;
  * @return boolean: True if AJAX is supported.
  */
 var getResponse = function (
-  url,       // string:    The URL to request data from.
-  data,      // object|:   Data to post. The method is automagically "POST" if data is truey, otherwise "GET".
+  url,       // string:    The URL to request a response from.
+  body,      // object|:   Data to post. The method is automagically "POST" if body is truey, otherwise "GET".
   onSuccess, // function|: Callback to run on success. `onSuccess(response, request)`.
   onFailure, // function|: Callback to run on failure. `onFailure(response, request)`.
   evalJson   // boolean|:  Whether to evaluate the response as JSON.
 ) {
-  // If the optional data argument is omitted, shuffle it out.
-  if (isFunction(data)) {
+  // If the optional body argument is omitted, shuffle it out.
+  if (isFunction(body)) {
     evalJson = onFailure;
     onFailure = onSuccess;
-    onSuccess = data;
-    data = 0;
+    onSuccess = body;
+    body = 0;
   }
   var request;
   if (window.XMLHttpRequest) {
@@ -69,38 +69,58 @@ var getResponse = function (
     request.onreadystatechange = function() {
       if (request.readyState == 4) {
         --getResponse._WAITING;
-        var isSuccess = (request.status == 200);
+        var status = request.status;
+        var isSuccess = (status == 200);
         var callback = isSuccess ?
           onSuccess || globalResponseSuccessHandler :
           onFailure || globalResponseFailureHandler;
         var response = request.responseText;
         if (evalJson) {
           var object;
-          try {
-            // Trick Uglify into thinking there's no eval.
-            var e = window.eval;
-            e('eval.J=' + response);
-            object = e.J;
+          if (status) {
+            try {
+              // Trick Uglify into thinking there's no eval.
+              var e = window.eval;
+              e('eval.J=' + response);
+              object = e.J;
+            }
+            catch (e) {
+              //+env:dev
+              error('Could not parse JSON: "' + response + '"');
+              //-env:dev
+              object = {_ERROR: '_BAD_JSON', _TEXT: response};
+            }
           }
-          catch (e) {
-            //+env:dev
-            error('Could not parse JSON: "' + response + '"');
-            //-env:dev
-            object = {_ERROR: 'Invalid JSON', _TEXT: response};
+          else {
+            object = {_ERROR: '_OFFLINE'};
           }
+          object._STATUS = status;
           object.request = request;
           response = object;
         }
         callback(response, request);
       }
     };
-    request.open(data ? 'POST' : 'GET', url, true);
+    request.open(body ? 'POST' : 'GET', url, true);
     request.setRequestHeader('x-requested-with', 'XMLHttpRequest');
-    if (data) {
+    if (body) {
       request.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
     }
     getResponse._WAITING = (getResponse._WAITING || 0) + 1;
-    request.send(data || null);
+
+    // Record the original request URL.
+    request.url = url;
+
+    // TODO: Populate request.query with URL query params.
+
+    // If it's a post, record the post body.
+    if (body) {
+      request.body = body;
+    }
+
+    //
+    request._TIME = new Date();
+    request.send(body || null);
   }
   return true;
 };
@@ -110,12 +130,12 @@ var getResponse = function (
  * @return boolean: True if AJAX is supported.
  */
 var getJson = function (
-  url,       // string:    The URL to request data from.
-  data,      // object|:   Data to post. The method is automagically "POST" if data is truey, otherwise "GET".
+  url,       // string:    The URL to request a response from.
+  body,      // object|:   Data to post. The method is automagically "POST" if body is truey, otherwise "GET".
   onSuccess, // function|: Callback to run on success. `onSuccess(response, request)`.
   onFailure  // function|: Callback to run on failure. `onFailure(response, request)`.
 ) {
-  return getResponse(url, data, onSuccess, onFailure, true);
+  return getResponse(url, body, onSuccess, onFailure, true);
 };
 var DEFAULT_ANIMATION_FRAME_COUNT = 40;
 var DEFAULT_ANIMATION_FRAME_DELAY = 20;
--- scripts/ajax.js
+++ scripts/ajax.js
@@ -10,18 +10,18 @@ var globalResponseFailureHandler = doNothing;
  * @return boolean: True if AJAX is supported.
  */
 var getResponse = function (
-  url,       // string:    The URL to request data from.
-  data,      // object|:   Data to post. The method is automagically "POST" if data is truey, otherwise "GET".
+  url,       // string:    The URL to request a response from.
+  body,      // object|:   Data to post. The method is automagically "POST" if body is truey, otherwise "GET".
   onSuccess, // function|: Callback to run on success. `onSuccess(response, request)`.
   onFailure, // function|: Callback to run on failure. `onFailure(response, request)`.
   evalJson   // boolean|:  Whether to evaluate the response as JSON.
 ) {
-  // If the optional data argument is omitted, shuffle it out.
-  if (isFunction(data)) {
+  // If the optional body argument is omitted, shuffle it out.
+  if (isFunction(body)) {
     evalJson = onFailure;
     onFailure = onSuccess;
-    onSuccess = data;
-    data = 0;
+    onSuccess = body;
+    body = 0;
   }
   var request;
   if (window.XMLHttpRequest) {
@@ -35,38 +35,58 @@ var getResponse = function (
     request.onreadystatechange = function() {
       if (request.readyState == 4) {
         --getResponse._WAITING;
-        var isSuccess = (request.status == 200);
+        var status = request.status;
+        var isSuccess = (status == 200);
         var callback = isSuccess ?
           onSuccess || globalResponseSuccessHandler :
           onFailure || globalResponseFailureHandler;
         var response = request.responseText;
         if (evalJson) {
           var object;
-          try {
-            // Trick Uglify into thinking there's no eval.
-            var e = window.eval;
-            e('eval.J=' + response);
-            object = e.J;
+          if (status) {
+            try {
+              // Trick Uglify into thinking there's no eval.
+              var e = window.eval;
+              e('eval.J=' + response);
+              object = e.J;
+            }
+            catch (e) {
+              //+env:dev
+              error('Could not parse JSON: "' + response + '"');
+              //-env:dev
+              object = {_ERROR: '_BAD_JSON', _TEXT: response};
+            }
           }
-          catch (e) {
-            //+env:dev
-            error('Could not parse JSON: "' + response + '"');
-            //-env:dev
-            object = {_ERROR: 'Invalid JSON', _TEXT: response};
+          else {
+            object = {_ERROR: '_OFFLINE'};
           }
+          object._STATUS = status;
           object.request = request;
           response = object;
         }
         callback(response, request);
       }
     };
-    request.open(data ? 'POST' : 'GET', url, true);
+    request.open(body ? 'POST' : 'GET', url, true);
     request.setRequestHeader('x-requested-with', 'XMLHttpRequest');
-    if (data) {
+    if (body) {
       request.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
     }
     getResponse._WAITING = (getResponse._WAITING || 0) + 1;
-    request.send(data || null);
+
+    // Record the original request URL.
+    request.url = url;
+
+    // TODO: Populate request.query with URL query params.
+
+    // If it's a post, record the post body.
+    if (body) {
+      request.body = body;
+    }
+
+    //
+    request._TIME = new Date();
+    request.send(body || null);
   }
   return true;
 };
@@ -76,10 +96,10 @@ var getResponse = function (
  * @return boolean: True if AJAX is supported.
  */
 var getJson = function (
-  url,       // string:    The URL to request data from.
-  data,      // object|:   Data to post. The method is automagically "POST" if data is truey, otherwise "GET".
+  url,       // string:    The URL to request a response from.
+  body,      // object|:   Data to post. The method is automagically "POST" if body is truey, otherwise "GET".
   onSuccess, // function|: Callback to run on success. `onSuccess(response, request)`.
   onFailure  // function|: Callback to run on failure. `onFailure(response, request)`.
 ) {
-  return getResponse(url, data, onSuccess, onFailure, true);
+  return getResponse(url, body, onSuccess, onFailure, true);
 };
